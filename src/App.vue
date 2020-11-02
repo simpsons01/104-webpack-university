98a2dde2b555aecdbcd465c3c7b690eec60cab5c
simpsons01 <39054010+simpsons01@users.noreply.github.com>
2020-11-03 00:33:49 +0800

    add image

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,10 @@
   <div id="app">
     <router-link to="/about">about</router-link>
     <router-view></router-view>
-    <img src="~assets/webpack.png" />
-    <img :src="imgSrc" />
     swefwefe
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      imgSrc: ""
-    }
-  },
-  async mounted() {
-    const { default: webpackPng } = await import("assets/webpack.png")
-    this.imgSrc = webpackPng
-  }
-}
+export default {}
 </script>

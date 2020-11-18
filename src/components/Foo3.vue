c6c3bfbf0c23a7602df54bcd724bc15fae31eb00
simpsosns01 <ilikekobe01@gmail.com>
2020-11-19 02:42:34 +0800

    chore(CPM-0002) 新增Foo2 && Foo3

---
+++ src/components/Foo3.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Foo">
+    Foo3
+  </div>
+</template>
+
+<script>
+import { add } from "utility/add.js"
+export default {
+  name: "Foo3",
+  mounted() {
+    add(2, 3)
+  }
+}
+</script>
